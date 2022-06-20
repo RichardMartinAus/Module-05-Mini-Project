266331518aedc6a09b2fc6332acf6e974bbce1d9
Craig <craig.skicko@gmail.com>
2022-06-20 21:03:47 +1000

    Added function to capture form inputs and pass to addItemToTable function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 $( "form" ).submit(function( event ) {
     event.preventDefault();
     var projectName = $("#project-name").val();
-    console.log(projectName);
+    var projectType = $("#project-type").val();
+    var hourlyWage = $("#hourly-wage").val();
+    var dueDate = $("#due-date").val();
+    
+    addItemToTable(projectName, projectType, hourlyWage, dueDate);
   });
+
+  function addItemToTable(x, y, z, a){
+    
+  }
